8fa2b17718e69dcdc054ee4b60ff3b10e48ac1bb
panzer <rocco92@hotmail.it>
2015-05-06 20:34:39 +0200

    Added The Eclipse project

---
+++ workspace/RecommenderSystem/src/User.java
@@ -0,0 +1,32 @@
+
+public class User {
+	private String name;
+	private int age;
+	private String sex;
+	private String country;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}
